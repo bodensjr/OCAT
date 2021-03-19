41a8a4d176274d91229abfcec854caf7ee28e9ee
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-18 22:15:29 -0400

    Added code to insert data into Database using bookshelf

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -3,7 +3,25 @@ const { Assessments } = require(`../Database`);
 exports.submit = (assessment) => {
   return new Promise(async (resolve, reject) => { //eslint-disable-line
     try {
-      //use the bookshelf model Assessments from API/src/microservices/Database to save the assessment data in the PostgreSQL database
+      const Date = new Date();
+      const DateCreated = `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+
+      var answers = { 
+        'cat_name': assessment.Name,
+        'cat_date_of_birth':assessment.DateOfBirth,
+        'instrument': assessment.instrument,
+        'score': assessment.score,
+        'risk_level': assessment.riskLevel,
+        'created_at': DateCreated,
+        'deleted_at': null
+      }
+      
+        var document = await new Assessments(answers).save().catch(function (e) {
+        resolve(document);
+      });
+
+      resolve(document);
+
 
       resolve();
     } catch (err) {
