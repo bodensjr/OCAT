1576f67077fa0c0b71640c87e9e5826f84aabb83
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-20 15:54:45 -0400

    Update index.js

--- API/src/microservices/Assessment-Service/index.js
+++ API/src/microservices/Assessment-Service/index.js
@@ -6,7 +6,7 @@ exports.submit = (assessment) => {
       const Date = new Date();
       const DateCreated = `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 
-      var answers = { 
+      const answers = { 
         'cat_name': assessment.Name,
         'cat_date_of_birth':assessment.DateOfBirth,
         'instrument': assessment.instrument,
