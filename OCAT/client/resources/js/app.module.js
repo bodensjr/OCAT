8afac0dc5ff782626f30e5c239c55315bc335650
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-24 16:30:13 -0400

    Update app.module.js

--- OCAT/client/resources/js/app.module.js
+++ OCAT/client/resources/js/app.module.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './app/App.component';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById(`app-root`));
