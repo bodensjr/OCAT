9f0ab9cbe6782369f7f568a2182298668416ed4e
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-03-31 20:41:21 -0400

    Added the necessary import statements for List.js

---
+++ OCAT/client/resources/js/components/Assessments/list.js
@@ -0,0 +1,3 @@
+import React, { useState, useEffect } from 'react';
+import { AssessmentService } from '../shared/services/assessment.service';
+import { useTable, useSortBy, useFilters } from 'react-table';
