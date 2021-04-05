607790e6834e5ad7bb9fcad35e2fd6dec526ef3a
bodensjr <70085015+bodensjr@users.noreply.github.com>
2021-04-04 22:49:06 -0400

    Update bulletin.js

--- OCAT/client/resources/js/components/Dashboard/bulletin.js
+++ OCAT/client/resources/js/components/Dashboard/bulletin.js
@@ -13,14 +13,24 @@ export class DashboardBulletin extends React.Component {
     const { } = this.state;
 
     return (
-        <>
-            <div>
-              <h1 >OCAT Dashboard</h1>
-              <NavLink to="/assessment/new">New</NavLink>
-              {/* <button href="/assessment/list">List</button> */}
+            <div className="container mt-3">
+              <div className="justify-content-md-center">
+                <div className="col-12 col-md-8">
+                  <div className="row">
+                    <h1 >OCAT Dashboard</h1>
+                  </div>
+                  <div className="row">
+                    <div className="col-auto">
+                      <NavLink to="/assessment/new">New</NavLink>
+                    </div>
+                    <div className="col-auto">
+                      <NavLink to="/assessment/list">List</NavLink>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <hr />
             </div>
-        </>
       );
   }
 }
